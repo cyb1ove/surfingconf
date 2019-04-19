6098c91d67ef67ac38858d3e0bf8b089863c7143
Андрей <cyb1ove.ua@gmail.com>
2019-04-19 13:14:05 +0300

    Add friction setting

--- surfingconf.js
+++ surfingconf.js
@@ -1,6 +1,7 @@
 /* Begin configuration */
 
-settings.scroolStepSize = 110;
+settings.scrollStepSize = 100;
+settings.scrollFriction = 20;
 settings.showProxyInStatusBar = true;
 settings.showModeStatus = true;
 settings.focusOnSaved = false;
