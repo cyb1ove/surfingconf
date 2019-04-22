6eac900d7b8d67f7e3e630c986c93dcb61bd09ba
Андрей <cyb1ove.ua@gmail.com>
2019-04-22 10:21:04 +0300

    Set australian proxy setting

--- surfingconf.js
+++ surfingconf.js
@@ -60,26 +60,7 @@ unmap('spi');
 
 /* Proxy settings */
 
-map('=pau', ':setProxy au.proxy.competera.net:3128', 0, "");
-map('=pde', ':setProxy de.proxy.competera.net:3128', 0, "");
-map('=pdk', ':setProxy 217.61.239.52:3128', 0, "");
-map('=pes', ':setProxy es.proxy.competera.net:3128', 0, "");
-map('=pfi', ':setProxy fi.proxy.competera.net:3128', 0, "");
-map('=pfr', ':setProxy fr.proxy.competera.net:3128', 0, "");
-map('=pie', ':setProxy ie.proxy.competera.net:3128', 0, "");
-map('=pin', ':setProxy in.proxy.competera.net:3128', 0, "");
-map('=pit', ':setProxy it.proxy.competera.net:3128', 0, "");
-map('=pnl', ':setProxy nl.proxy.competera.net:3128', 0, "");
-map('=pno', ':setProxy no.proxy.competera.net:3128', 0, "");
-map('=ppt', ':setProxy pt.proxy.competera.net:3128', 0, "");
-map('=pru', ':setProxy ru.proxy.competera.net:3128', 0, "");
-map('=pse', ':setProxy se.proxy.competera.net:3128', 0, "");
-map('=puk', ':setProxy uk.proxy.competera.net:3128', 0, "");
-map('=pus', ':setProxy us.proxy.competera.net:3128', 0, "");
-map('=pmc', ':setProxyMode clear', 0, "");
-map('=pma', ':setProxyMode always', 0, "");
-
 mapkey('spau', '#16Set australian proxy', () => {
-    Normal.feedkeys('=pma');
-    Normal.feedkeys('=pau');
+    Front.executeCommand("setProxyMode always");
+    Front.executeCommand("setProxy au.proxy.competera.net:3128");
 });
