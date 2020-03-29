089f44c8e154c00b9d2375e1f20120626857459a
cyb1ove <cyb1ove.ua@gmail.com>
2020-03-29 10:45:38 +0200

    Update surfingconf.js

--- surfingconf.js
+++ surfingconf.js
@@ -21,9 +21,7 @@ unmap("p");
 unmap("<Alt-p>");
 unmap("yf");
 unmap("ys");
-unmap("y1");
-unmap("y2");
-unmap("f");
+unmap("ya");
 
 mapkey("t", '#3Choose a tab', function() {
     Front.chooseTab();
@@ -66,31 +64,32 @@ mapkey('zk', '#3zoom out', function() {
     });
 });
 mapkey('yk', '#8Copy contract information from CRM', function() {
+                             
     var table_left = document.getElementById('w1').getElementsByTagName('tbody')[0];
     var table_right = document.getElementById('w2').getElementsByTagName('tbody')[0];
     var number = document.getElementById('section-title');
-    var phone =  table_right.getElementsByTagName('tr')[10].getElementsByTagName('td')[0].innerHTML.split(",")[0]
     
     Clipboard.write(table_right.getElementsByTagName('tr')[0].getElementsByTagName('td')[0].innerHTML + ";" 
                   + table_right.getElementsByTagName('tr')[1].getElementsByTagName('td')[0].innerHTML + " " 
                   + table_right.getElementsByTagName('tr')[2].getElementsByTagName('td')[0].innerHTML + ";" 
                   + number.textContent.replace(/\((.*?)\)|\s/g, "") + ";"
                   + table_left.getElementsByTagName('tr')[5].getElementsByTagName('td')[0].innerHTML + ";"
-                  + phone)
+                  + table_right.getElementsByTagName('tr')[10].getElementsByTagName('td')[0].innerHTML.split(",")[0])
 })
 mapkey('yj', '#8Copy contract information from CRM', function() {
+                             
     var table_left = document.getElementById('w1').getElementsByTagName('tbody')[0];
     var table_right = document.getElementById('w2').getElementsByTagName('tbody')[0];
     var number = document.getElementById('section-title');
-    var phone =  table_right.getElementsByTagName('tr')[11].getElementsByTagName('td')[0].innerHTML.split(",")[0]
     
     Clipboard.write(table_right.getElementsByTagName('tr')[0].getElementsByTagName('td')[0].innerHTML + ";" 
                   + table_right.getElementsByTagName('tr')[1].getElementsByTagName('td')[0].innerHTML + " " 
                   + table_right.getElementsByTagName('tr')[2].getElementsByTagName('td')[0].innerHTML + ";" 
                   + number.textContent.replace(/\((.*?)\)|\s/g, "") + ";"
                   + table_left.getElementsByTagName('tr')[5].getElementsByTagName('td')[0].innerHTML + ";"
-                  + phone)
+                  + table_right.getElementsByTagName('tr')[11].getElementsByTagName('td')[0].innerHTML.split(",")[0])
 })
+
 mapkey('yP', '#8Copy marketer calls information', function() {
     Hints.create('div[id="call-history-grid"] > table > tbody > tr',  function(call) {
         let marketer = call.querySelectorAll('td')[3].textContent;
@@ -99,15 +98,16 @@ mapkey('yP', '#8Copy marketer calls information', function() {
     })
 })
 mapkey('yS', '#8Copy statistics', function() {
-    const names = ['Гаврилова Мария', 'Филиппова Наталья', 'Трохименко Людмила', 'Лысенко Карина', 
-                   'Наталия Бардык', 'Бардык Наталия', 'Ковезина', 'Шустрова', 'Федорченко Николай', 
+    const names = ['Гаврилова Мария', 'Филиппова_1L Наталья', 'Филиппова_2L Наталья', 
+                   'Трохименко Людмила_1L', 'Трохименко Людмила_2L', 'Лысенко_1L Карина', 
+                   'Лысенко_2L Карина', 'Бардык Наталия_1L', 'Бардык Наталия_2L', 
+                   'Ковезина', 'Шустрова', 'Федорченко Николай', 
                    'Третяк_1L Ирина', 'Третяк_2L Ирина', 'Руденко', 'Харьковская'];
     var statistic = [];
     names.forEach((name) => {
         statistic.push({marketer: name, Zgoda: 0, Pozniej: 0, Pomy: 0, Odmowa: 0, exists: false})
     })
-    /*var count_phone_numbers = document_url("http://marketell.pl/marketell3/marketell/projects/customers/id/" +
-                                            document.location.href.split('/').slice(-1)[0])*/
+    
     document.getElementsByClassName("items table table-striped table-condensed")[0].querySelectorAll('tbody > tr').forEach((call) => {
         var marketer = call.getElementsByTagName('td')[3].textContent;
         var result = call.getElementsByTagName('td')[5].textContent;
@@ -131,46 +131,108 @@ mapkey('yS', '#8Copy statistics', function() {
             if (marketer_stat['exists'] == true) {
                 table += (marketer_stat['marketer'] != 0 ? marketer_stat['marketer'] : 0) + "\t" +
                          (marketer_stat['Zgoda'] != 0 ? marketer_stat['Zgoda'] : 0) + "\t" +
-                         (marketer_stat['Pomy'] != 0 ? marketer_stat['Pomyleka'] : 0) + "\t" +
+                         (marketer_stat['Pomy'] != 0 ? marketer_stat['Pomy'] : 0) + "\t" +
                          (marketer_stat['Pozniej'] != 0 ? marketer_stat['Pozniej'] : 0) + "\t" +
                          (marketer_stat['Odmowa'] != 0 ? marketer_stat['Odmowa'] : 0) + "\t" + total + "\n";
             }
         })
         return table;
     }
-    
-    /*function makeHttpObject() {
-        try {return new XMLHttpRequest();}
-        catch (error) {}
-        try {return new ActiveXObject("Msxml2.XMLHTTP");}
-        catch (error) {}
-        try {return new ActiveXObject("Microsoft.XMLHTTP");}
-        catch (error) {}
-
-        throw new Error("Could not create HTTP request object.");
-    }
-var request = makeHttpObject();
-request.open("GET", "http://marketell.pl/marketell3/marketell/projects/view/id/1573", true);
-request.send(null);
-request.onreadystatechange = function() {
-  if (request.readyState == 4)
-    alert(document);
-};*/
 
     Clipboard.write(statistic_table())
 });
 
-mapkey('fC', '#8Open all contracts', function() {
-    document.getElementsByClassName('table table-hover table-striped table-condensed table-bordered table-small kv-grid-table kv-table-wrap')[0].querySelectorAll('tbody > tr').forEach((contract) => {
+mapkey('yaS', '#8Copy statistics', function() {
+    const names = ['Гаврилова Мария', 'Филиппова_1L Наталья', 'Филиппова_2L Наталья', 
+                   'Трохименко Людмила_1L', 'Трохименко Людмила_2L', 'Лысенко_1L Карина', 
+                   'Лысенко_2L Карина', 'Бардык Наталия_1L', 'Бардык Наталия_2L', 
+                   'Ковезина', 'Шустрова', 'Федорченко Николай', 
+                   'Третяк_1L Ирина', 'Третяк_2L Ирина', 'Руденко', 'Харьковская'];
+    var statistic = [];
+
+    Clipboard.read((response) => {
+        clipboard = response.data
+        names.forEach((name) => {
+            var exist = false;
+            clipboard.split('\n').forEach((marketer_hist, i, arr) => {
+                var marketer_hist = marketer_hist.split('\t')
+                if (marketer_hist[0].includes(name)) {
+                    statistic.push({marketer: name, Zgoda: parseInt(marketer_hist[1]), 
+                                    Pozniej: parseInt(marketer_hist[3]), 
+                                    Pomy: parseInt(marketer_hist[2]), 
+                                    Odmowa: parseInt(marketer_hist[4]), 
+                                    exists: true})
+                    exist = true;
+                } else if (i == arr.length - 1 && exist == false) {
+                    statistic.push({marketer: name, Zgoda: 0, Pozniej: 0, Pomy: 0, Odmowa: 0, exists: false})
+                }
+            })
+        })
+    })
+    
+    document.getElementsByClassName("items table table-striped table-condensed")[0].querySelectorAll('tbody > tr').forEach((call) => {
+        var marketer = call.getElementsByTagName('td')[3].textContent;
+        var result = call.getElementsByTagName('td')[5].textContent;
+        var comment = call.getElementsByTagName('td')[14].textContent;
+        statistic.forEach((marketer_stat, i, arr) => {
+            if (marketer.includes(marketer_stat.marketer)) {
+                Object.keys(marketer_stat).forEach((result_stat) => {
+                    if (result.includes(result_stat)) {
+                        marketer_stat[result_stat]++;
+                        marketer_stat['exists'] = true;
+                    }
+                })
+            }
+        })
+    }); 
+
+    var statistic_table = function() {
+        let table = "";
+        statistic.forEach((marketer_stat) => {
+            let total = marketer_stat['Zgoda'] + marketer_stat['Pomy'] + marketer_stat['Pozniej'] + marketer_stat['Odmowa']
+            if (marketer_stat['exists'] == true) {
+                table += (marketer_stat['marketer'] != 0 ? marketer_stat['marketer'] : 0) + "\t" +
+                         (marketer_stat['Zgoda'] != 0 ? marketer_stat['Zgoda'] : 0) + "\t" +
+                         (marketer_stat['Pomy'] != 0 ? marketer_stat['Pomy'] : 0) + "\t" +
+                         (marketer_stat['Pozniej'] != 0 ? marketer_stat['Pozniej'] : 0) + "\t" +
+                         (marketer_stat['Odmowa'] != 0 ? marketer_stat['Odmowa'] : 0) + "\t" + total + "\n";
+            }
+        })
+        return table;
+    }
+
+    Clipboard.write(statistic_table())
+});
+
+mapkey('Fc', '#8Open all contracts', function() {
+    /*document.getElementsByClassName('table table-hover table-striped table-condensed table-bordered table-small kv-grid-table kv-table-wrap')[0].querySelectorAll('tbody > tr').forEach((contract) => {
         contracts_url = document.location.host + contract.querySelector('td[data-col-seq="0"] a').getAttribute('href');
+        tabOpenLink(contracts_url);
+    })*/
+    document.getElementsByClassName('v-datatable v-table theme--light')[0].querySelectorAll('tbody > tr').forEach((contract) => {
+        contracts_url = contract.querySelector('span a').getAttribute('href');
         tabOpenLink(contracts_url);
     })
 })
-mapkey('fc', 'Open contract', function() {
-    Hints.create("table.kv-table-wrap > tbody > tr > td > a[class = 'no-pjax']",
-                   Hints.dispatchMouseClick, {})
-})
 
+mapkey('yT', '#8Total contracts', function() {
+    let calls = []
+    document.getElementsByClassName('items table table-striped table-condensed')[0].querySelectorAll('tbody > tr').forEach((call) => {
+        calls.push(call.querySelectorAll('td')[7].textContent)
+    })
+    Clipboard.write([...new Set(calls)].length)
+})
+mapkey('yaT', '#8Add total contracts', function() {
+    var calls = []
+    var total;
+    document.getElementsByClassName('items table table-striped table-condensed')[0].querySelectorAll('tbody > tr').forEach((call) => {
+        calls.push(call.querySelectorAll('td')[7].textContent)
+    })
+    Clipboard.read((response) => {
+        total = parseInt(response.data) + [...new Set(calls)].length
+    })
+    Clipboard.write(total)
+})
 map('J', 'd');
 map('K', 'e');
 
