67ec1639b3007068d39990d1e9ea9493e6d805b8
cyb1ove <cyb1ove.ua@gmail.com>
2021-07-31 10:09:33 +0300

    Update surfingconf.js

--- surfingconf.js
+++ surfingconf.js
@@ -4,20 +4,4 @@ settings.showModeStatus = true;
 settings.scrollStepSize = 250;
 settings.focusOnSaved = false;
 
-unmap('<Ctrl-h>')
-unmap('h');
-unmap('l');
-unmap('f');
-unmap('r');
-unmap('i');
-unmap('I');
-unmap('d');
-unmap('q');
-unmap('s');
-unmap('x');
-unmap('X');
-unmap('p');
-unmap('m');
-unmap('e');
-unmap('n');
-unmap('yt');
+unmap('<Ctrl-h>');
